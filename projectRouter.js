6007379f44f25a28e01a54e9149584baf411ca5d
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-14 11:53:20 -0400

    Updated GET request for projects

--- projectRouter.js
+++ projectRouter.js
@@ -1,7 +1,18 @@
 const express = require('express');
 
-const ProjectData = require('./data/helpers/projectModel');
+const projectData = require('./data/helpers/projectModel');
 
 const router = express.Router();
+
+// READ - send back a list of all projects
+router.get('/', (req, res) => {
+    projectData.get()
+    .then(allProjects => {
+        res.json(allProjects);
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'The project information could not be retrieved.' })
+    })
+})
 
 module.exports = router;
